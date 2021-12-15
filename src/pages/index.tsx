85ed861ea4b5843e89b8357e951763984f6c0ea4
Flávio Arêas <vflavio9@gmail.com>
2021-12-15 08:38:00 -0300

    feat: fetch and display posts in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { FiUser, FiCalendar } from 'react-icons/fi';
 
 import Header from '../components/Header';
@@ -29,7 +32,7 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   return (
     <>
       <Head>
@@ -40,103 +43,74 @@ export default function Home() {
         <Header />
 
         <ul>
-          <li className={styles.post}>
-            <Link href="/post/teste">
-              <a>
-                <strong>Como utilizar hooks</strong>
-              </a>
-            </Link>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          {postsPagination.results.map(post => (
+            <li key={post.uid} className={styles.post}>
+              <Link href={`/posts/${post.uid}`}>
+                <a>
+                  <strong>{post.data.title}</strong>
+                </a>
+              </Link>
+              <p>{post.data.subtitle}</p>
 
-            <div className={styles.postInfo}>
-              <time>
-                <FiCalendar size={20} />
-                14 Dez 2021
-              </time>
+              <div className={styles.postInfo}>
+                <time>
+                  <FiCalendar size={20} />
+                  {post.first_publication_date}
+                </time>
 
-              <small>
-                <FiUser size={20} />
-                Flávio Arêas
-              </small>
-            </div>
-          </li>
-          <li className={styles.post}>
-            <strong>Como utilizar hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-            <div>
-              <time>
-                <FiCalendar size={20} />
-                14 Dez 2021
-              </time>
-
-              <small>
-                <FiUser size={20} />
-                Flávio Arêas
-              </small>
-            </div>
-          </li>
-          <li className={styles.post}>
-            <strong>Como utilizar hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-            <div>
-              <time>
-                <FiCalendar size={20} />
-                14 Dez 2021
-              </time>
-
-              <small>
-                <FiUser size={20} />
-                Flávio Arêas
-              </small>
-            </div>
-          </li>
-          <li className={styles.post}>
-            <strong>Como utilizar hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-            <div>
-              <time>
-                <FiCalendar size={20} />
-                14 Dez 2021
-              </time>
-
-              <small>
-                <FiUser size={20} />
-                Flávio Arêas
-              </small>
-            </div>
-          </li>
-          <li className={styles.post}>
-            <strong>Como utilizar hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-            <div>
-              <time>
-                <FiCalendar size={20} />
-                14 Dez 2021
-              </time>
-
-              <small>
-                <FiUser size={20} />
-                Flávio Arêas
-              </small>
-            </div>
-          </li>
+                <small>
+                  <FiUser size={20} />
+                  {post.data.author}
+                </small>
+              </div>
+            </li>
+          ))}
         </ul>
 
-        <button type="button" className={styles.nextPage}>
-          Carregar mais posts
-        </button>
+        {postsPagination.next_page && (
+          <button type="button" className={styles.nextPage}>
+            Carregar mais posts
+          </button>
+        )}
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+        next_page: postsResponse.next_page,
+      },
+    },
+  };
+};
